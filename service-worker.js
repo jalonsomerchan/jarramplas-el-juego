965fc307ae197d7a10bc475eae4e353d6a814e99
jalonsomerchan <jalonsomerchan@gmail.com>
2026-05-06 18:07:29 +0200

    Update cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "jarramplas-v20260506-6";
+const CACHE_VERSION = "jarramplas-v20260506-7";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const ASSET_CACHE = `${CACHE_VERSION}-assets`;
 
