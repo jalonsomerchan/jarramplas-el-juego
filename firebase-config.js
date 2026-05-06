270cf7797c7ad5768cc4f15aa2a8b091e31f8994
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-06 10:26:14 +0200

    feat: add Firebase leaderboard config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const FIREBASE_LEADERBOARD_ENABLED = false;
+
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+export const FIREBASE_LEADERBOARD_COLLECTION = "leaderboard";
